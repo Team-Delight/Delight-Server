46f30fc7a9dc39624da963f8212c038b04c978cf
DevBloo <devchanghwan@gmail.com>
2021-07-26 16:29:11 +0900

    [#1] init: project init

---
+++ src/main/java/com/team/delightserver/DelightserverApplication.java
@@ -0,0 +1,13 @@
+package com.team.delightserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelightserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelightserverApplication.class, args);
+	}
+
+}
