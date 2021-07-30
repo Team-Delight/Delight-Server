7146a419750e859be2ff8308a2d7c78030dfd434
dohyung97022 <dohyung97022@gmail.com>
2021-07-30 14:10:38 +0900

    [#5] feature : jwt 유틸 추가

---
+++ src/main/java/com/team/delightserver/util/JWTUtil.java
@@ -0,0 +1,55 @@
+package com.team.delightserver.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Created by Doe
+ * @Date: 2021/07/30
+ */
+
+@Component
+public class JWTUtil {
+    private String SECRET_KEY = "secret";
+    private SignatureAlgorithm ALGORITHM = SignatureAlgorithm.HS256;
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+
+    public <T extends Object> T extractFromClaims(Claims claims, String key){
+        Object value = claims.get(key);
+        return (T) value;
+    }
+
+    public Boolean isTokenValid(String token) {
+        try{
+            return !extractAllClaims(token).getExpiration().before(new Date());
+        } catch (ExpiredJwtException exception){
+            return false;
+        }
+    }
+
+    public String generateToken(Map<String, Object> payload, String subject, Long jwtDueMillis) {
+        Map<String, Object> header = new HashMap<>();
+        header.put("typ", "JWT");
+        header.put("alg", ALGORITHM.getValue());
+
+        Date expireDate = new Date(new Date().getTime() + jwtDueMillis);
+
+        return Jwts.builder()
+                .setHeader(header)
+                .setClaims(new HashMap<>(payload))
+                .setSubject(subject)
+                .setExpiration(expireDate)
+                .signWith(ALGORITHM, SECRET_KEY)
+                .compact();
+    }
+}
