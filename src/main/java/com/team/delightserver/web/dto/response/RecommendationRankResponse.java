1b6d4160fad6e9972e4be5fa171bdb92d671a01d
dohyung97022 <dohyung97022@gmail.com>
2021-08-11 21:28:12 +0900

    [#29] refactor: @QueryProjection 불필요

--- src/main/java/com/team/delightserver/web/dto/response/RecommendationRankResponse.java
+++ src/main/java/com/team/delightserver/web/dto/response/RecommendationRankResponse.java
@@ -1,22 +1,20 @@
 package com.team.delightserver.web.dto.response;
 
-import com.querydsl.core.annotations.QueryProjection;
 import lombok.*;
 
 /**
- * @CreateBy:Min
- * @Date: 2021/08/05
+ * @CreateBy: Min, Doe
+ * @Date: 2021/08/05, 2021/08/11
  */
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public class RecommendationRankResponse {
     private Long categoryId;
-    private String name;
     private int recommendedCnt;
+    private String name;
     private String imgUrl;
 
-    @QueryProjection
     public RecommendationRankResponse(int count, String name, String imgUrl, Long id) {
         this.recommendedCnt = count;
         this.name = name;
