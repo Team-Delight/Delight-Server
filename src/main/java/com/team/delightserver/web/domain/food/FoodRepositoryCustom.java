209d10dd46bdd4b032075b2c41d417f623bf3404
dohyung97022 <dohyung97022@gmail.com>
2021-08-11 21:26:20 +0900

    [#29] feature: querydsl 로 findFoodByTagId 생성

---
+++ src/main/java/com/team/delightserver/web/domain/food/FoodRepositoryCustom.java
@@ -0,0 +1,15 @@
+package com.team.delightserver.web.domain.food;
+
+import com.team.delightserver.web.dto.response.TagRelatedFoodsResponse;
+
+import java.util.List;
+
+/**
+ * @Created by Doe
+ * @Date: 2021/08/10
+ */
+
+public interface FoodRepositoryCustom {
+
+    List<TagRelatedFoodsResponse> findAllByTagId(Long id);
+}
