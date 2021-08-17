56be52f61133176fe7bde7285c5dce31f2f0e539
dohyung97022 <dohyung97022@gmail.com>
2021-08-18 01:29:26 +0900

    [#70] feature : tag 카테고리에 따라 결과 나오는 api

--- src/main/java/com/team/delightserver/web/domain/tag/TagRepository.java
+++ src/main/java/com/team/delightserver/web/domain/tag/TagRepository.java
@@ -1,11 +1,16 @@
 package com.team.delightserver.web.domain.tag;
 
+import com.team.delightserver.util.enumclass.TagType;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 /**
  * @CreateBy: Doe
- * @Date: 2021/08/13
+ * @Date: 2021/08/17
+ * @ModifiedDate: 2021/08/18
  */
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    List<Tag> findAllByType(TagType tagType);
 }
