fc4906acad411c55b4b652a260f890cf0707fa74
DevBloo <devchanghwan@gmail.com>
2021-08-18 17:04:48 +0900

    [#79] feat: Delight Server Time Zone Asia/Seoul 로 변경

--- src/main/java/com/team/delightserver/DelightserverApplication.java
+++ src/main/java/com/team/delightserver/DelightserverApplication.java
@@ -1,5 +1,7 @@
 package com.team.delightserver;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,6 +10,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class DelightserverApplication {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DelightserverApplication.class, args);
 	}
