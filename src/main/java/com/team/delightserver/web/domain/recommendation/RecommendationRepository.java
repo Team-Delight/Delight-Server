53388c7090346d2773f20f83f5a0aaaf0d90257b
SEOLMIN KWON <stat_kwon@naver.com>
2021-08-18 15:30:25 +0900

    [#74] feat: 연관관계 변경에 따른 카테고리별 랭킹 조회 수정
    1. FoodRecommendationImpl -> RecommendationImpl로 변경 후 연관관계에 맞게 쿼리 수정
    2. 시간 로직 재수정

--- src/main/java/com/team/delightserver/web/domain/recommendation/RecommendationRepository.java
+++ src/main/java/com/team/delightserver/web/domain/recommendation/RecommendationRepository.java
@@ -1,12 +1,12 @@
 package com.team.delightserver.web.domain.recommendation;
 
-import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * @CreateBy:Min
  * @Date: 2021/08/02
+ * @ModifiedDate: 2021/08/18
  */
 
-public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
+public interface RecommendationRepository extends JpaRepository<Recommendation, Long>, RecommendationRepositoryCustom {
 }
---
+++ src/main/java/com/team/delightserver/web/domain/recommendation/RecommendationRepositoryImpl.java
@@ -0,0 +1,91 @@
+package com.team.delightserver.web.domain.recommendation;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.team.delightserver.web.dto.response.RecommendationRankResponse;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static com.querydsl.core.types.ExpressionUtils.count;
+import static com.team.delightserver.web.domain.category.QCategory.category;
+import static com.team.delightserver.web.domain.food.QFood.food;
+import static com.team.delightserver.web.domain.recommendation.QRecommendation.recommendation;
+
+/**
+ * @CreateBy:Min
+ * @Date: 2021/08/18
+ */
+
+@RequiredArgsConstructor
+public class RecommendationRepositoryImpl implements RecommendationRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<RecommendationRankResponse> findAllTopTenByCategoryId(Long id) {
+
+        JPAQuery<RecommendationRankResponse> query = queryFactory
+                .selectDistinct(Projections.constructor(RecommendationRankResponse.class,
+                        ExpressionUtils.as(
+                                JPAExpressions
+                                        .select(count(recommendation))
+                                        .from(recommendation)
+                                        .where(recommendation.food.id.eq(food.id)),
+                                "recommendedCnt"),
+                        food.name,
+                        food.imgUrl,
+                        food.category.id))
+                .from(recommendation)
+                .innerJoin(recommendation.food, food)
+                .innerJoin(food.category, category);
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+        isValidTime(booleanBuilder);
+        whetherCategoryId(id, booleanBuilder);
+
+        return query.where(booleanBuilder)
+                .orderBy(new OrderSpecifier<>(Order.DESC,
+                        Expressions.numberPath(Long.class, "recommendedCnt")))
+                .limit(10)
+                .fetch();
+    }
+
+    private void isValidTime(BooleanBuilder booleanBuilder) {
+
+        LocalDateTime nowTime = LocalDateTime.now();
+        LocalDateTime startAmToday = LocalDateTime.now().toLocalDate().atStartOfDay();
+        LocalDateTime endAmToday = LocalDateTime.now().toLocalDate().atStartOfDay().plusHours(12);
+        LocalDateTime startYesterday = startAmToday.minusDays(1);
+
+
+        if (nowTime.isAfter(startAmToday) && nowTime.isBefore(endAmToday)) {
+            booleanBuilder.and(recommendation.createdAt
+                    .between(startYesterday, startAmToday));
+        }
+        if (nowTime.isAfter(endAmToday)) {
+            booleanBuilder.and(recommendation.createdAt
+                    .goe(startAmToday));
+        }
+    }
+
+    private void whetherCategoryId(Long id, BooleanBuilder booleanBuilder) {
+        if (id != null && id != 0) {
+            booleanBuilder.and(categoryIdEq(id));
+        }
+    }
+
+    private BooleanExpression categoryIdEq(Long id) {
+        return id != null ? food.category.id.eq(id) : null;
+    }
+}
