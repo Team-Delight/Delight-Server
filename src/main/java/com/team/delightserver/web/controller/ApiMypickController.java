f34311403fcac93bafd04b9c23268534bf4150bc
dohyung97022 <dohyung97022@gmail.com>
2021-08-19 10:32:43 +0900

    [#76] fix : .httpstatus -> .created 로 변경

--- src/main/java/com/team/delightserver/web/controller/ApiMypickController.java
+++ src/main/java/com/team/delightserver/web/controller/ApiMypickController.java
@@ -4,9 +4,10 @@ import com.team.delightserver.security.annotation.CurrentUser;
 import com.team.delightserver.security.oauth2.OAuth2UserProvider;
 import com.team.delightserver.service.MypickService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 /**
  * @Created by Doe
@@ -24,6 +25,6 @@ public class ApiMypickController {
     @PostMapping("")
     public ResponseEntity<Void> saveMypick(@CurrentUser OAuth2UserProvider user, @RequestParam Long foodId) {
         mypickService.saveMypick(user, foodId);
-        return ResponseEntity.status(HttpStatus.CREATED).body(null);
+        return ResponseEntity.created(URI.create("/api/mypicks")).build();
     }
 }
